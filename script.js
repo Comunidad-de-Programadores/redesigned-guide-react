3c5e24ec46b837bd963455f008cbee7a2ed5f1d2
himuraxkenji <aaron.ariperto@gmail.com>
2023-03-26 16:17:52 -0300

    add card templating from javascript

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
+
 const INACTIVE = 'inactive'
 
 
@@ -41,9 +43,75 @@ const  toggleCartAside = () => {
 }
 
 
-
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMovileMenu)
 menuCartIcon.addEventListener('click', toggleCartAside)
 
+const productsList = []
+productsList.push({
+    name: 'Round shelf',
+    price: 120,
+    image: 'assets/round-shelf.webp'
+})
+productsList.push({
+    name: 'Retro refrigerator',
+    price: 240,
+    image: 'assets/retro-refrigerator.png'
+})
+productsList.push({
+    name: 'Round shelf',
+    price: 120,
+    image: 'assets/round-shelf.webp'
+})
+productsList.push({
+    name: 'Retro refrigerator',
+    price: 240,
+    image: 'assets/retro-refrigerator.png'
+})
+productsList.push({
+    name: 'Round shelf',
+    price: 120,
+    image: 'assets/round-shelf.webp'
+})
+productsList.push({
+    name: 'Retro refrigerator',
+    price: 240,
+    image: 'assets/retro-refrigerator.png'
+})
 
+
+for (product of productsList){
+    const productCard = document.createElement('div')
+    productCard.classList.add('product-card')
+
+    const productImg = document.createElement('img')
+    productImg.setAttribute('src', product.image)
+    
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info')
+
+    const productInfoDiv = document.createElement('div')
+
+    const productPrice = document.createElement('p')
+    const productName = document.createElement('p')
+
+    productPrice.innerText = `$ ${product.price}`
+    productName.innerText = product.name
+
+    productInfoDiv.appendChild(productPrice)
+    productInfoDiv.appendChild(productName)
+
+    const productInfoFigure = document.createElement('figure')
+    const productImgCart = document.createElement('img')
+    productImgCart.setAttribute('src', 'assets/icons/bt_add_to_cart.svg')
+
+    productInfoFigure.appendChild(productImgCart)
+
+    productInfo.appendChild(productInfoDiv)
+    productInfo.appendChild(productInfoFigure)
+
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+
+    cardsContainer.appendChild(productCard)
+}
