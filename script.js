77344f4d5653b5fa865b208ddfbcf52149afcb1e
himuraxkenji <aaron.ariperto@gmail.com>
2023-03-26 15:38:48 -0300

    add my shopping cart

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 const INACTIVE = 'inactive'
 
 
@@ -13,8 +15,14 @@ const toggleMovileMenu = () => {
     mobileMenu.classList.toggle(INACTIVE)
 }
 
+const  toggleCartAside = () => {
+    aside.classList.toggle(INACTIVE)
+}
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMovileMenu)
+menuCartIcon.addEventListener('click', toggleCartAside)
 
 
