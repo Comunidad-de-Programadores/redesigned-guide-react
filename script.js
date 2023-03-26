9316c78fd3c437737d9b8ef27e8ae5417041ad90
himuraxkenji <aaron.ariperto@gmail.com>
2023-03-26 15:04:51 -0300

    add interactive menu with movile

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 const INACTIVE = 'inactive'
 
 
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle(INACTIVE)
 }
+
+const toggleMovileMenu = () => {
+    mobileMenu.classList.toggle(INACTIVE)
+}
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMovileMenu)
+
+
